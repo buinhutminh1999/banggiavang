6cfcf6d885058a8bcacb812e0320e647ea7f3b00
buinhutminh1999 <buinhutminh1999@gmail.com>
2023-03-17 19:29:01 +0700

    update

--- src/Component/HomeComponent.jsx
+++ src/Component/HomeComponent.jsx
@@ -7,10 +7,10 @@ import TableShowData from './TableShowData';
 export default function HomeComponent() {
     return (
         <div className='container-fluid' style={{ minHeight: '100vh', cursor: 'pointer', backgroundImage: 'linear-gradient(to right, #141E30 0%, #243B55 51%, #141E30 100%', display: 'flex', justifyContent: 'center', flexDirection: 'column', fontWeight: '800' }} >
-            <div className=' text-danger header' >
+            <div className='text-danger header' >
                 <div className='d-flex' style={{ position: 'relative' }}>
                     <div class="content">
-                        <h1 className='dntn' style={{ color: '#00d2ff' }} >DNTN</h1>
+                        <h1 className='dntn'>DNTN</h1>
                     </div>
                     <h1 style={{ color: '#F09819', position: 'absolute' }} className='text-center w-100'>
                         <p style={{ fontSize: '50px' }} class="animate-charcter">TIỆM VÀNG</p>
